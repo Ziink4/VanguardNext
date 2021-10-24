99a5beab111c5e43172c39221687d5e1db534f82
Florian Brenot <Ziink4@users.noreply.github.com>
2021-10-24 12:26:15 +0200

    Display all 3 axes

--- Visualization/main.py
+++ Visualization/main.py
@@ -1,75 +1,83 @@
 import time
 import subprocess
+from collections import deque
+
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import re
 from threading import Thread
-from queue import Empty, LifoQueue
 
-fig = plt.figure()
+fig = plt.figure("Raw Sensor Data")
 ax1 = fig.add_subplot(1, 1, 1)
-data,  = ax1.plot([0], [0])
-text = ax1.text(0.97, 0.97, "", transform=ax1.transAxes, ha="right", va="top")
+x_data,  = ax1.plot([0], [0], 'r', label='X - roll')
+y_data,  = ax1.plot([0], [0], 'g', label='Y - pitch')
+z_data,  = ax1.plot([0], [0], 'b', label='Z - yaw')
+text = ax1.text(0.97, 0.03, "", transform=ax1.transAxes, ha="right", va="top")
+ax1.legend()
+
 plt.ylim(-32768, 32767)
 
 MAX_X = 1000
 plt.xlim(0, MAX_X)
 
+x_values = []
 y_values = []
-p, q, t = None, None, None
-last_time = {0: time.time()}
+z_values = []
+proc = None
+LINES = deque(maxlen=1)
+LAST_TIME = {0: time.time()}
 
 
 def animate(i):
-    global y_values
-    if len(y_values) < MAX_X:
-        y_values.append(i)
+    global x_values, y_values, z_values
+    if len(x_values) < MAX_X:
+        x_values.append(i[0])
+        y_values.append(i[1])
+        z_values.append(i[2])
     else:
-        y_values = y_values[1:] + [i]
+        x_values = x_values[1:] + [i[0]]
+        y_values = y_values[1:] + [i[1]]
+        z_values = z_values[1:] + [i[2]]
 
-    data.set_data(range(len(y_values)), y_values)
+    x_data.set_data(range(len(x_values)), x_values)
+    y_data.set_data(range(len(y_values)), y_values)
+    z_data.set_data(range(len(z_values)), z_values)
 
     new_time = time.time()
-    text.set_text("{0:.2f} fps".format(1. / (new_time - last_time[0])))
-    last_time.update({0:new_time})
+    text.set_text("{0:.2f} fps".format(1. / (new_time - LAST_TIME[0])))
+    LAST_TIME.update({0: new_time})
 
 
-def enqueue_output(out, queue):
+def enqueue_output(out, lines):
     for line in iter(out.readline, b''):
-        queue.put(line)
+        lines.append(line)
+
     out.close()
 
 
 def open_gyro():
     args = ["D:\\Documents\\Helicoptere RC\\Segger RTT over ST-Link\\strtt.exe", "-ram", "4"]
-    global p, q, t
     p = subprocess.Popen(args, stdout=subprocess.PIPE, bufsize=1, text=True)
-    q = LifoQueue()
-    t = Thread(target=enqueue_output, args=(p.stdout, q))
-    t.daemon = True
-    t.start()
+    Thread(target=enqueue_output, args=(p.stdout, LINES), daemon=True).start()
+    return p
 
 
 def read_gyro():
-    global q
+    regex = re.compile(r"\x1b\[0m\x1b\[32mINFO: GYRO: +(-?\d*) +(-?\d*) +(-?\d*)\n")
     while True:
-        try:
-            line = q.get_nowait()
-        except Empty:
-            pass
-        else:
-            print(q.qsize())
-            result = re.findall(r"\x1b\[0m\x1b\[32mINFO: GYRO: +(-?\d*)\n", line)
+        if LINES:
+            line = LINES[-1]
+            result = re.findall(regex, line)
             if result:
-                yield int(result[0])
+                yield int(result[0][0]), int(result[0][1]), int(result[0][2])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    open_gyro()
+    proc = open_gyro()
 
     ani = FuncAnimation(fig, animate, read_gyro, interval=0)
 
     plt.show()
 
-    p.wait()
+    proc.wait()
