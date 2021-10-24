3493a729e166595898f2c8408aa8f75c8c9b8926
Florian Brenot <Ziink4@users.noreply.github.com>
2021-10-24 18:00:09 +0200

    Add retries to I2C

--- Core/MPU6050/eMD6/eMPL/inv_mpu.c
+++ Core/MPU6050/eMD6/eMPL/inv_mpu.c
@@ -124,12 +124,34 @@ static inline int reg_int_cb(struct int_param_s *int_param)
 #define INV_MPU6050_I2C_TIMEOUT 1000
 static inline HAL_StatusTypeDef INV_MPU_I2C_Write(unsigned char slave_addr, unsigned char reg_addr, unsigned char length, unsigned char *data)
 {
-  return HAL_I2C_Mem_Write(&hi2c1, slave_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, data, length, INV_MPU6050_I2C_TIMEOUT);
+  HAL_StatusTypeDef ret = HAL_ERROR;
+  char retries = 0;
+  while (ret && retries++ < 4)
+  {
+    ret = HAL_I2C_Mem_Write(&hi2c1, slave_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, data, length, INV_MPU6050_I2C_TIMEOUT);
+    if (ret)
+    {
+      HAL_Delay(50);
+    }
+  }
+
+  return ret;
 }
 
 static inline HAL_StatusTypeDef INV_MPU_I2C_Read(unsigned char slave_addr, unsigned char reg_addr, unsigned char length, unsigned char *data)
 {
-  return HAL_I2C_Mem_Read(&hi2c1, slave_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, data, length, INV_MPU6050_I2C_TIMEOUT);
+  HAL_StatusTypeDef ret = HAL_ERROR;
+  char retries = 0;
+  while (ret && retries++ < 4)
+  {
+    ret = HAL_I2C_Mem_Read(&hi2c1, slave_addr << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, data, length, INV_MPU6050_I2C_TIMEOUT);
+    if (ret)
+    {
+      HAL_Delay(50);
+    }
+  }
+
+  return ret;
 }
 
 static inline void INV_MPU_Get_MS(unsigned long *out)
